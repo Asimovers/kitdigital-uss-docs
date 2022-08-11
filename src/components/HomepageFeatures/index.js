39b097eff9797efed2a1227835f5c0e8a4a652fb
Felipe Mancini <fmancini.cl@gmail.com>
2022-08-10 20:57:25 -0400

    fotos y variables

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,6 +6,7 @@ const FeatureList = [
     description: "Valores de marca USS sobre los que hemos construido nuestro sistema de diseño digital.",
     CTAlink: "/docs/category/valores-de-marca",
     CTAlabel: "Guía de marca",
+    img: "/img/uss-marca.jpeg",
   },
   {
     title: "Diseño",
@@ -13,22 +14,24 @@ const FeatureList = [
       "En esta sección podrás encontrar los fundamentos de diseño del kit USS y sus recursos disponibles en Figma.",
     CTAlink: "/docs/category/diseño",
     CTAlabel: "Diseño con el kit",
+    img: "/img/designers.jpg",
   },
   {
     title: "Desarrollo",
     description: "Introducción al kit digital USS para desarrolladores y proveedores internos/externos.",
     CTAlink: "/docs/category/desarrolladores",
     CTAlabel: "Desarrollo con el kit",
+    img: "/img/developers.jpg",
   },
 ];
 
-function Feature({ CTAlink, CTAlabel, title, description }) {
+function Feature({ CTAlink, CTAlabel, title, description, img }) {
   return (
     <div className="uss-card uss-card--background-alt" style={{ width: "30%" }}>
       <img
         lazy
         className={"card-img"}
-        src={"/img/placeholder-img.png"}
+        src={img}
         alt={"dummyimage"}
       />
       <div className={"uss-card__body"}>
